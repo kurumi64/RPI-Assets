aa0b3d868d8be0aaa1f26da1e6af489ccdf10415
Kiki Adi Pamungkas <92570748+kurumi64@users.noreply.github.com>
2021-12-14 23:47:25 +0700

    Create test-lcd-I2C-20x4.ino

---
+++ Testing/test-lcd-I2C-20x4.ino
@@ -0,0 +1,25 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup(){
+  lcd.init(); // mulai lcd
+  lcd.backlight(); // lampu backlight lcd menyala
+  lcd.clear(); // menghapus konten di lcd
+}
+
+void loop(){
+
+    lcd.setCursor(0,0);
+    lcd.print("KIKI ADI PAMUNGKAS");
+    
+    lcd.setCursor(0,1);
+    lcd.print("D4 RPL 1B");
+    
+    lcd.setCursor(0,2);
+    lcd.print("RPI? JUARA!");
+    
+    lcd.setCursor(0,3);
+    lcd.print("SENTOLOP :v");
+    
+}
